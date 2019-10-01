9f4f6a808ea955b8ce11a9c8ecb43a6b0f044fd3
rachita23 <rachita18044@iiitd.ac.in>
2019-10-01 12:57:19 +0530

    research lab page commited

---
+++ app/src/main/java/com/team13/campusmitra/R_Lab.java
@@ -0,0 +1,21 @@
+package com.team13.campusmitra;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class R_Lab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r_lab);
+        /*RecyclerView project_list;
+        project_list = (RecyclerView) findViewById(R.id.proj_list);
+        project_list.setLayoutManager(new LinearLayoutManager(this));
+        String[] project_items = {"Project 1", "Project 2", "Project 3", "Project 4", "Project 5", "Project 6","Project 7","Project 8","Project 9","Project 10"};
+        project_list.setAdapter(new Projects_Adapter(project_items));*/
+    }
+}
