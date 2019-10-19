a02f0e3a2de1f3073db01f08de9870c153bab15d
rohit18115 <rohit18115@iiitd.ac.in>
2019-10-19 22:41:16 +0530

    Student Profile in Progress

--- app/src/main/java/com/team13/campusmitra/SplashActivity.java
+++ app/src/main/java/com/team13/campusmitra/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(getApplicationContext(), LabsRecyclerView.class);
+        Intent intent = new Intent(getApplicationContext(), SelectCoursesRecyclerView.class);
 
         startActivity(intent);
         finish();
