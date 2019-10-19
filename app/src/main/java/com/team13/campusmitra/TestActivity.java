2a67ea93607757df05815e8c57610dda2cfb2e18
iamnikhilgola <nikhil18129@iiitd.ac.in>
2019-10-19 23:56:49 +0530

    OCR integrated with Firebase

--- app/src/main/java/com/team13/campusmitra/TestActivity.java
+++ app/src/main/java/com/team13/campusmitra/TestActivity.java
@@ -1,23 +1,34 @@
 package com.team13.campusmitra;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.team13.campusmitra.actions.LoginAction;
 import com.team13.campusmitra.actions.SignupAction;
+import com.team13.campusmitra.dataholder.Room;
 import com.team13.campusmitra.dataholder.Student;
+import com.team13.campusmitra.firebaseassistant.FirebaseRoomHelper;
+
+import java.util.ArrayList;
 
 public class TestActivity extends AppCompatActivity {
 
     Button testButton;
+
     DatabaseReference databaseReference;
     DatabaseReference courseRefrence;
+    ArrayList<Room> rooms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +42,37 @@ public class TestActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //insert("nikks95","nikks@gmail.com","Mt18129",0);
                 //doSignup("nikhilgola21@gmail.com","12345678");
-                doLogin("nikhilgola21@gmail.com","12345678");
+                //doLogin("nikhilgola21@gmail.com","12345678");
+                //addRoom();
+                launchOCR();
             }
         });
     }
 
-    private void doSignup(String email,String pass){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        rooms = new ArrayList<>();
+        FirebaseRoomHelper helper = new FirebaseRoomHelper();
+        DatabaseReference reference = helper.getReference();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                rooms.clear();
+                for (DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    Room room = snapshot.getValue(Room.class);
+                    rooms.add(room);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void doSignup(String email, String pass){
         SignupAction action = new SignupAction(this,email,pass,this);
         int i = action.doSignup();
     }
@@ -52,6 +88,22 @@ public class TestActivity extends AppCompatActivity {
        // Student student = new Student(uid,username,useremail,"2",rollnum);
         //databaseReference.child(uid).setValue(student);
         Toast.makeText(this,"Added Success",Toast.LENGTH_LONG).show();
+    }
+    private void addRoom(){
+        System.out.println(rooms);
+        Room room = new Room();
+        room.setCapacity(30);
+        room.setRoomNumber("A-519");
+        room.setRoomBuilding("Rnd Block");
+        room.setRoomType(0);
+        FirebaseRoomHelper helper = new FirebaseRoomHelper();
+        helper.addRoom(this,room);
+        System.out.println(rooms);
+    }
+    private void launchOCR(){
+        Intent intent = new Intent(this,OCRActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/com/team13/campusmitra/dataholder/Room.java
+++ app/src/main/java/com/team13/campusmitra/dataholder/Room.java
@@ -14,8 +14,63 @@ public class Room implements Serializable {
     private String labName;
     private int systemCount;
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getRoomNotes() {
+        return roomNotes;
+    }
+
+    public void setRoomNotes(String roomNotes) {
+        this.roomNotes = roomNotes;
+    }
+
+    public String getLabName() {
+        return labName;
+    }
+
+    public void setLabName(String labName) {
+        this.labName = labName;
+    }
+
+    public int getSystemCount() {
+        return systemCount;
+    }
+
+    public void setSystemCount(int systemCount) {
+        this.systemCount = systemCount;
+    }
+
+    public Room(String roomID, String roomNumber, String roomBuilding, int roomType, String roomImageURL, String roomDescription, int capacity, String roomNotes, String labName, int systemCount) {
+        this.roomID = roomID;
+        this.roomNumber = roomNumber;
+        this.roomBuilding = roomBuilding;
+        this.roomType = roomType;
+        this.roomImageURL = roomImageURL;
+        this.roomDescription = roomDescription;
+        this.capacity = capacity;
+        this.roomNotes = roomNotes;
+        this.labName = labName;
+        this.systemCount = systemCount;
+    }
 
     public Room() {
+        this.roomID = null;
+        this.roomNumber = null;
+        this.roomBuilding = null;
+        this.roomType = -1;
+        this.roomImageURL = null;
+        this.roomDescription = null;
+        this.capacity = 0;
+        this.roomNotes = null;
+        this.labName = null;
+        this.systemCount = 0;
+
     }
 
     public String getRoomNumber() {
@@ -73,5 +128,10 @@ public class Room implements Serializable {
         this.roomType = roomType;
         this.roomImageURL = roomImageURL;
         this.roomDescription = roomDescription;
+    }
+    @Override
+    public String toString(){
+        String s = "["+getRoomNumber()+" ,"+getRoomBuilding()+"]";
+        return s;
     }
 }
