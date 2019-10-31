74a3227f07f46f5cfef292ae2d8dfbe93491035a
rohit18115 <rohit18115@iiitd.ac.in>
2019-11-01 01:56:31 +0530

    Upload resume done

--- app/src/main/java/com/team13/campusmitra/LabsRecyclerView.java
+++ app/src/main/java/com/team13/campusmitra/LabsRecyclerView.java
@@ -7,6 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
 
 import com.team13.campusmitra.adaptors.LabsRecyclerViewAdaptor;
 import com.team13.campusmitra.dataholder.Room;
@@ -14,7 +19,7 @@ import com.team13.campusmitra.dataholder.Room;
 import java.util.ArrayList;
 
 public class LabsRecyclerView extends AppCompatActivity {
-
+    LabsRecyclerViewAdaptor adapter;
     private static final String TAG = "LabsRecyclerView";
 
     private ArrayList<Room> items = new ArrayList<>();
@@ -42,11 +47,35 @@ public class LabsRecyclerView extends AppCompatActivity {
     private void initRecycler() {
         Log.d(TAG, "initComponents: started");
         RecyclerView recyclerView = findViewById(R.id.labs_recycler_view);
-        LabsRecyclerViewAdaptor adapter = new LabsRecyclerViewAdaptor(items, this);
+        adapter = new LabsRecyclerViewAdaptor(items, this);
         recyclerView.setAdapter(adapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         DividerItemDecoration did = new DividerItemDecoration(this,layoutManager.getOrientation());
         recyclerView.addItemDecoration(did);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_selectcourse, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.SCapp_bar_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
     }
 }
--- app/src/main/java/com/team13/campusmitra/StudentProfile.java
+++ app/src/main/java/com/team13/campusmitra/StudentProfile.java
@@ -1,15 +1,34 @@
 package com.team13.campusmitra;
 
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.team13.campusmitra.dataholder.Student;
 import com.team13.campusmitra.dataholder.User;
 import com.team13.campusmitra.firebaseassistant.FirebaseStudentHelper;
@@ -20,6 +39,12 @@ public class StudentProfile extends AppCompatActivity implements View.OnClickLis
     TextView interests;
     TextView rollNo;
     Button select_courses;
+    Button upload, next;
+    TextView selected_file;
+    FirebaseStorage storage;
+    FirebaseDatabase database;
+    Uri pdfUri;
+    ProgressDialog progressDialog;
 
     static final int PICK_CONTACT_REQUEST = 1;
 
@@ -28,6 +53,9 @@ public class StudentProfile extends AppCompatActivity implements View.OnClickLis
         department = findViewById(R.id.department);
         interests = findViewById(R.id.interests);
         rollNo = findViewById(R.id.rollNumber);
+        upload = findViewById(R.id.uploadresume);
+        next = findViewById(R.id.SPnext);
+        selected_file = findViewById(R.id.display_selected_file);
     }
 
     private void getStudentObject() {
@@ -51,10 +79,14 @@ public class StudentProfile extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
+        initComponents();
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
         select_courses = findViewById(R.id.selectcourses);
         select_courses.setOnClickListener(this);
         display_courses = (TextView)findViewById(R.id.display_courses_id);
-
+        upload.setOnClickListener(this);
+        next.setOnClickListener(this);
         select_courses.setOnClickListener(this);
         if (savedInstanceState == null) {
             Bundle courses = getIntent().getExtras();
@@ -74,7 +106,80 @@ public class StudentProfile extends AppCompatActivity implements View.OnClickLis
             case R.id.selectcourses :
                 Intent intent1 = new Intent(StudentProfile.this, SelectCoursesRecyclerView.class);
                 startActivityForResult(intent1, PICK_CONTACT_REQUEST);
+                break;
+            case R.id.uploadresume:
+                if(ContextCompat.checkSelfPermission(StudentProfile.this, Manifest.permission.READ_EXTERNAL_STORAGE )== PackageManager.PERMISSION_GRANTED){
+                    select_pdf();
+
+                }
+                else{
+                    ActivityCompat.requestPermissions(StudentProfile.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 9);
+                }
+                break;
+            case R.id.SPnext:
+
         }
+    }
+
+    private void uploadFile(Uri pdfUri) {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("Uploading File");
+        progressDialog.setProgress(0);
+        progressDialog.show();
+        final String fileName = System.currentTimeMillis()+"";
+        StorageReference storageReference = storage.getReference();
+
+        storageReference.child("Uploads").child(fileName).putFile(pdfUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                String url = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                DatabaseReference reference = database.getReference();
+                reference.child(fileName).setValue(url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                            Toast.makeText(StudentProfile.this,"File successfully uploaded",Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(StudentProfile.this,"File not uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(StudentProfile.this,"File not uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                int currentProgess = (int) (100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                progressDialog.setProgress(currentProgess);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==9 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            select_pdf();
+
+        }
+        else{
+            Toast.makeText(StudentProfile.this,"please provide permission...",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void select_pdf() {
+
+        Intent intentfile = new Intent();
+        intentfile.setType("application/pdf");
+        intentfile.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentfile,86);
     }
 
     @Override
@@ -90,6 +195,21 @@ public class StudentProfile extends AppCompatActivity implements View.OnClickLis
                     display_courses.setText(t);
             }
         }
+        else if (requestCode==86 && resultCode==RESULT_OK && data!=null) {
+            pdfUri = data.getData();
+            if(pdfUri!=null){
+                Log.d("lolo", "uploaded  granted");
+                uploadFile(pdfUri);
+            }
+            else{
+                Toast.makeText(StudentProfile.this,"Select a file....",Toast.LENGTH_SHORT).show();
+            }
+            selected_file.setText("A file is selected");
+        }
+        else{
+            Toast.makeText(StudentProfile.this,"Please select a file....",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
