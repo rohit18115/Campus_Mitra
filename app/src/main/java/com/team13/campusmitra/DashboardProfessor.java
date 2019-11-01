64895435add6544ee8cfcaecff5371a1e2c9067a
yogesh18140 <yogesh18140@iiitd.ac.in>
2019-11-01 21:30:45 +0530

    Minor fix

--- app/src/main/java/com/team13/campusmitra/DashboardProfessor.java
+++ app/src/main/java/com/team13/campusmitra/DashboardProfessor.java
@@ -1,22 +1,80 @@
 package com.team13.campusmitra;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.team13.campusmitra.dataholder.User;
+import com.team13.campusmitra.firebaseassistant.FirebaseUserHelper;
 
 public class DashboardProfessor extends AppCompatActivity {
+
+
+    ImageView image;
+
+
+    protected void loadImage() {
+
+        image = findViewById(R.id.imgViewProf);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        final String uid = auth.getCurrentUser().getUid();
+        FirebaseUserHelper helper = new FirebaseUserHelper();
+        DatabaseReference reference = helper.getReference();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    if(user.getUserId().equals(uid)) {
+                        Log.d("lololo", "onDataChange: " + user.getUserLastName());
+                        String gender = user.getGender();
+                        if(gender.equals("Male")) {
+                            Glide.with(getApplicationContext())
+                                    .asBitmap()
+                                    .load(user.getImageUrl())
+                                    .placeholder(R.drawable.blankboy)
+                                    .into(image);
+                        }
+                        else{
+                            Glide.with(getApplicationContext())
+                                    .asBitmap()
+                                    .load(user.getImageUrl())
+                                    .placeholder(R.drawable.blankgirl)
+                                    .into(image);
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_professor);
+        loadImage();
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
